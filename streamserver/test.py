13a6d014009c7005578968d7189b04bbeca09566
wonleing@gmail.com <wonleing@gmail.com@fdab4ecc-152b-7705-98f9-e3542dc6df4c>
2012-07-21 12:06:07 +0000

    new logic completed

--- streamserver/test.py
+++ streamserver/test.py
@@ -1,8 +1,8 @@
-import xmlrpclib
+#!/usr/bin/python
+import xmlrpclib, os
+# run following command in other terminal, CTRL+C when you want to finish test
+# i=0;while ( true );do echo $i >> /var/ftp/pub/vwnexpch.mp4;((i+=1));done
+
 s=xmlrpclib.ServerProxy("http://127.0.0.1:8000")
-s.genSegment("xpwluetf", "Rider test video")
-s.updateSegment("xpwluetf", "2")
-s.updateSegment("xpwluetf", "3")
-s.updateSegment("xpwluetf", "4")
-s.updateSegment("xpwluetf", "5")
-s.finishRecord("testdel")
+if s.genSegment("vwnexpch", "test video with number filler"):
+    s.finishRecord("vwnexpch")
