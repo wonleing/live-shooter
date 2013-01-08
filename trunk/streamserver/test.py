483a4ddd40dbcc94f65d9baae06edb41823ed0ec
wonleing@gmail.com <wonleing@gmail.com@fdab4ecc-152b-7705-98f9-e3542dc6df4c>
2013-01-08 10:20:42 +0000

    database, xmlrpc api and unit test completed

--- trunk/streamserver/test.py
+++ trunk/streamserver/test.py
@@ -1,10 +1,41 @@
 #!/usr/bin/python
 import xmlrpclib, os, time
+uname = "test@sina.com"
+usns = "sina"
+videotitle = "My first video"
+snsid = "fowqjfsadkfjaldf"
+vsample = "/home/leon/download/raiders.mp4"
+ftpdir = "/var/ftp/pub/"
+s=xmlrpclib.ServerProxy("http://127.0.0.1:8000")
 
-s=xmlrpclib.ServerProxy("http://192.168.188.2:8000")
-filename = s.startRecord("mytitle","mydesc")
-os.system("cp /home/leon/download/smile.flv /var/ftp/pub/%s.mp4" %filename)
-print "upload completed"
-time.sleep(60)
-s.finishRecord(filename)
-print "Finished"
+userid = s.loginUser(uname, usns)
+print "create user:", userid
+
+videoid = s.genFilename()
+print "upload video file name is:", videoid
+
+if s.addTitle(userid, videoid, videotitle):
+    print "add tiltle %s for %s" %(videotitle, videoid)
+
+os.system("cp %s %s%s.mp4" %(vsample, ftpdir, videoid))
+url = s.finishUpload(videoid)
+print "Transcode completed, now you can watch %s" %url
+
+if s.shareVideo(videoid, snsid):
+    print "add sns string", snsid, "to video", videoid
+
+if s.likeVideo(userid, videoid):
+    print "user", userid, "liked video", videoid
+
+ul = s.getFollowing(userid)
+print "user", userid, "following these users:", str(ul)
+
+ul = s.getFollower(userid)
+print "user", userid, "is followed by these users:", str(ul)
+
+vl = s.getUserVideo(userid)
+print "user", userid, "has uploaded following video", str(vl)
+
+vl = s.getFeed(userid)
+print "user", userid, "feed list is:", str(vl)
+print "API testing finished!"
