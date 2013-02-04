2c21ca2d08e8dcea2853551ddaf3997268e968e5
wonleing@gmail.com <wonleing@gmail.com@fdab4ecc-152b-7705-98f9-e3542dc6df4c>
2013-02-04 12:58:55 +0000

    fix db data sync problem

---
+++ streamserver/insertdata_local.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+import xmlrpclib, sys 
+from ftplib import FTP
+
+if len(sys.argv) != 5:
+    print "usage: "+sys.argv[0]+" <sns_user_name> <nickname> <icon_link> <path_to_video>"
+    sys.exit(1)
+IP = "127.0.0.1"
+ftp = FTP(IP, "live", "shooter")
+uname = sys.argv[1]
+usns = uname.split('@')[1].split('.')[0]
+nickname = sys.argv[2]
+icon = sys.argv[3]
+videotitle = "%s`s first video" %nickname
+vsample = sys.argv[4]
+ftpdir = "/var/ftp/pub/"
+s=xmlrpclib.ServerProxy("http://%s:8000" %IP)
+
+userid = s.loginUser(uname, usns, nickname, icon)
+print "create user:", userid
+
+videoid = s.genFilename()
+print "upload video file name is:", videoid
+snsid = "FAKE_SNSID_OF_VIDEO_"+videoid
+
+if s.addTitle(userid, videoid, videotitle):
+    print "add tiltle %s for %s" %(videotitle, videoid)
+
+ftp.storbinary('STOR %s' %videoid, open(vsample, 'rb'))
+url = s.finishUpload(videoid)
+print "Transcode completed, now you can watch %s" %url
+
+if s.shareVideo(videoid, snsid):
+    print "add sns string", snsid, "to video", videoid
+
+if s.likeVideo(userid, videoid) and s.likeVideo(1, videoid):
+    print "Admin and user", userid, "liked video", videoid
+
+if s.followUser(userid, 1) and s.followVideo(1, videoid):
+    print "user", userid, "and Admin followed each other"
+
+print "Data inserting finished!"
--- streamserver/lsdb.py
+++ streamserver/lsdb.py
@@ -1,4 +1,5 @@
 DB_type = 'mysql'
+from contextlib import closing
 
 class DB:
     def __init__(self):
@@ -9,17 +10,11 @@ class DB:
         except:
             import sqlite3
             self.cx = sqlite3.connect("./liveshooter.db")
-        self.cu = self.cx.cursor()
-
-    def __del__(self):
-        try:
-            self.cx.close()
-        except:
-            pass
 
     def selectUserID(self, uname, usns):
-        self.cu.execute("select userid from user where username='%s' and sns='%s'" %(uname, usns))
-        ret = self.cu.fetchall()
+        with closing(self.cx.cursor()) as cu:
+            cu.execute("select userid from user where username='%s' and sns='%s'" %(uname, usns))
+            ret = cu.fetchall()
         if ret:
             return ret[0][0]
         return False
@@ -27,106 +22,122 @@ class DB:
     def createUser(self, uname, usns, nickname, icon):
         # All new created users are free, change user type with modifyUser
         try:
-            self.cu.execute("insert into user (username, sns, type, nickname, icon) values ('%s', '%s', 'free', '%s', '%s')" \
-            %(uname, usns, nickname, icon))
-            self.cx.commit()
+            with closing(self.cx.cursor()) as cu:
+                cu.execute("insert into user (username, sns, type, nickname, icon) values ('%s', '%s', 'free', '%s', '%s')" \
+                %(uname, usns, nickname, icon))
+                self.cx.commit()
         except:
             return False
         return self.selectUserID(uname, usns)
 
     def updateUser(self, userid, nickname, icon):
-        self.cu.execute("update user set nickname='%s', icon='%s' where userid=%d" %(nickname, icon, userid))
-        self.cx.commit()
+        with closing(self.cx.cursor()) as cu:
+            cu.execute("update user set nickname='%s', icon='%s' where userid=%d" %(nickname, icon, userid))
+            self.cx.commit()
         return True
 
     def addVideo(self, userid, videoid, videotitle):
         try:
-            self.cu.execute("insert into uservideo (userid, videoid) values (%d, '%s')" %(userid, videoid))
-            self.cu.execute("insert into video (videoid, title, snsid, score) values ('%s', '%s', '', 0)" %(videoid, videotitle))
-            self.cu.execute("insert into videocheck (videoid, status) values ('%s', 'new')" %videoid)
+            with closing(self.cx.cursor()) as cu:
+                cu.execute("insert into uservideo (userid, videoid) values (%d, '%s')" %(userid, videoid))
+                cu.execute("insert into video (videoid, title, snsid, score) values ('%s', '%s', '', 0)" %(videoid, videotitle))
+                cu.execute("insert into videocheck (videoid, status) values ('%s', 'new')" %videoid)
         except:
             return False
         return True
 
     def shareVideo(self, videoid, snsid):
-        self.cu.execute("update video set snsid='%s' where videoid='%s'" %(snsid, videoid))
-        self.cx.commit()
+        with closing(self.cx.cursor()) as cu:
+            cu.execute("update video set snsid='%s' where videoid='%s'" %(snsid, videoid))
+            self.cx.commit()
         return True
 
     def likeVideo(self, userid, videoid):
         try:
-            self.cu.execute("insert into userlike (userid, videoid) values (%d, '%s')" %(userid, videoid))
-            self.cx.commit()
+            with closing(self.cx.cursor()) as cu:
+                cu.execute("insert into userlike (userid, videoid) values (%d, '%s')" %(userid, videoid))
+                cu.execute("update video set score=score+1 where videoid='%s'" %videoid)
+                self.cx.commit()
         except:
             return False
-        self.cu.execute("update video set score=score+1 where videoid='%s'" %videoid)
-        self.cx.commit()
         return True
 
     def unlikeVideo(self, userid, videoid):
         try:
-            self.cu.execute("delete from userlike where userid=%d and videoid='%s'" %(userid, videoid))
-            self.cx.commit()
+            with closing(self.cx.cursor()) as cu:
+                cu.execute("delete from userlike where userid=%d and videoid='%s'" %(userid, videoid))
+                cu.execute("update video set score=score-1 where videoid='%s'" %videoid)
+                self.cx.commit()
         except:
             return False
-        self.cu.execute("update video set score=score-1 where videoid='%s'" %videoid)
-        self.cx.commit()
         return True
 
     def followUser(self, userid, targetid):
         try:
-            self.cu.execute("insert into followship (userid, following) values (%d, %d)" %(userid, targetid))
-            self.cx.commit()
+            with closing(self.cx.cursor()) as cu:
+                cu.execute("insert into followship (userid, following) values (%d, %d)" %(userid, targetid))
+                self.cx.commit()
         except:
             return False
         return True
 
     def unfollowUser(self, userid, targetid):
-        self.cu.execute("delete from followship where userid=%d and following=%d" %(userid, targetid))
-        self.cx.commit()
+        with closing(self.cx.cursor()) as cu:
+            cu.execute("delete from followship where userid=%d and following=%d" %(userid, targetid))
+            self.cx.commit()
         return True
 
     def getVideoUser(self, videoid):
-        self.cu.execute("select userid from uservideo where videoid='%s'" %videoid)
-        return self.cu.fetchall()[0][0]
+        with closing(self.cx.cursor()) as cu:
+            cu.execute("select userid from uservideo where videoid='%s'" %videoid)
+            ret = cu.fetchall()[0][0]
+        return ret
 
     def getUserVideo(self, userid):
-        self.cu.execute("select v.* from video as v,uservideo as uv where uv.videoid=v.videoid and uv.userid=%d \
-        order by createdate desc limit 100" %userid)
-        return self.cu.fetchall()
+        with closing(self.cx.cursor()) as cu:
+            cu.execute("select v.* from video as v,uservideo as uv where uv.videoid=v.videoid and uv.userid=%d \
+            order by createdate desc limit 100" %userid)
+            return cu.fetchall()
 
     def getUserProfile(self, userid):
-        self.cu.execute("select * from user where userid=%d" %userid) 
-        return self.cu.fetchall()[0]
+        with closing(self.cx.cursor()) as cu:
+            cu.execute("select * from user where userid=%d" %userid) 
+            return cu.fetchall()[0]
 
     def getLikeVideo(self, userid):
-        self.cu.execute("select v.* from video as v,userlike as ul where ul.videoid=v.videoid and ul.userid=%d \
-        order by createdate desc limit 100" %userid)
-        return self.cu.fetchall()
+        with closing(self.cx.cursor()) as cu:
+            cu.execute("select v.* from video as v,userlike as ul where ul.videoid=v.videoid and ul.userid=%d \
+            order by createdate desc limit 100" %userid)
+            return cu.fetchall()
 
     def getFollowing(self, userid):
-        self.cu.execute("select u.* from followship as f, user as u where f.userid=%d and f.following=u.userid" %userid)
-        return self.cu.fetchall()
+        with closing(self.cx.cursor()) as cu:
+            cu.execute("select u.* from followship as f, user as u where f.userid=%d and f.following=u.userid" %userid)
+            return cu.fetchall()
 
     def getFollower(self, userid):
-        self.cu.execute("select u.* from followship as f, user as u where f.following=%d and f.userid=u.userid" %userid)
-        return self.cu.fetchall()
+        with closing(self.cx.cursor()) as cu:
+            cu.execute("select u.* from followship as f, user as u where f.following=%d and f.userid=u.userid" %userid)
+            return cu.fetchall()
 
     def getFeed(self, userid):
         #mysql has a bug, cannot order by createdate here
-        self.cu.execute("select v.*, u.* from video as v, userlike as ul, user as u, uservideo as uv where ul.userid=%d and \
-        ul.videoid=v.videoid and uv.videoid=v.videoid and u.userid=uv.userid union \
-        select v.*, u.* from video as v, uservideo as uv, user as u where uv.videoid=v.videoid and u.userid=uv.userid and uv.userid in \
-        (select following from followship where userid=%d) limit 100" %(userid, userid))
-        return self.cu.fetchall()
+        with closing(self.cx.cursor()) as cu:
+            cu.execute("select v.*, u.* from video as v, userlike as ul, user as u, uservideo as uv where ul.userid=%d and \
+            ul.videoid=v.videoid and uv.videoid=v.videoid and u.userid=uv.userid union \
+            select v.*, u.* from video as v, uservideo as uv, user as u where uv.videoid=v.videoid and u.userid=uv.userid and uv.userid in \
+            (select following from followship where userid=%d) limit 100" %(userid, userid))
+            return cu.fetchall()
 
     def getTopUser(self, type, num):
-        self.cu.execute("select u.*, count(*) from user as u, followship as f where f.following in (select userid from user where type='%s') \
-        and u.userid=f.following group by f.following order by count(*) desc limit %d" %(type, num))
-        return self.cu.fetchall()
+        with closing(self.cx.cursor()) as cu:
+            cu.execute("select u.*, count(*) from user as u, followship as f where f.following in (select userid from user where type='%s') \
+            and u.userid=f.following group by f.following order by count(*) desc limit %d" %(type, num))
+            return cu.fetchall()
 
     def getTopVideo(self, date):
-        self.cu.execute("select v.*, u.* from video as v, user as u, uservideo as uv where v.videoid=uv.videoid and \
-        u.userid=uv.userid and v.createdate>'%s' order by v.score desc, v.createdate desc limit 100" %date)
-        return self.cu.fetchall()
+        with closing(self.cx.cursor()) as cu:
+            cu.execute("select v.*, u.* from video as v, user as u, uservideo as uv where v.videoid=uv.videoid and \
+            u.userid=uv.userid and v.createdate>'%s' order by v.score desc, v.createdate desc limit 100" %date)
+            return cu.fetchall()
 
