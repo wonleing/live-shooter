6cb9f7e19d50aa83d3dfa7d4d5284a0c69071570
wonleing@gmail.com <wonleing@gmail.com@fdab4ecc-152b-7705-98f9-e3542dc6df4c>
2013-04-11 11:15:08 +0000

    add login function

--- trunk/ls_website/liveshooter/views.py
+++ trunk/ls_website/liveshooter/views.py
@@ -4,16 +4,28 @@ from django.conf import settings
 from django.http import Http404, HttpResponse
 from liveshooter.models import Followship, Userlike, User, Uservideo, Video
 from ftplib import FTP
-import xmlrpclib, os
+import xmlrpclib, os, sina
 s=xmlrpclib.ServerProxy("%s:8000" %settings.XMLRPC_URL, encoding='latin-1')
+burl = 'http://54.248.182.51/'
+
+def _check_login(request, context):
+    if 'login_user' not in request.session:
+        request.session['login_user'] = ""
+    if 'login_id' not in request.session:
+        request.session['login_id'] = ""
+    context['login_user'] = request.session['login_user']
+    context['login_id'] = str(request.session['login_id'])
 
 def index(request):
     recommand_users = s.getRecommandUser(True)
     recommand_videos = s.getRecommandVideo(True)
     context = {
         'recommand_users': recommand_users,
-        'recommand_videos': recommand_videos
+        'recommand_videos': recommand_videos,
+        'login_user': request.session['login_user'],
+        'login_id': request.session['login_id']
     }
+    _check_login(request, context)
     return render(request, 'index.html', context)
 
 def user(request, userid):
@@ -22,10 +34,15 @@ def user(request, userid):
         user_videos = s.getUserVideo(int(userid), True)
     except:
         raise Http404
+    if 'login_user' not in request.session:
+        request.session['login_user'] = ""
+    if 'login_id' not in request.session:
+        request.session['login_id'] = ""
     context = {
         'up': user_profile,
         'user_videos': user_videos
     }
+    _check_login(request, context)
     return render(request, 'user.html', context)
 
 def follower(request, userid):
@@ -38,6 +55,7 @@ def follower(request, userid):
         'up': user_profile,
         'follower_list': follower_list,
     }
+    _check_login(request, context)
     return render(request, 'follower.html', context)
 
 def following(request, userid):
@@ -50,17 +68,21 @@ def following(request, userid):
         'up': user_profile,
         'following_list': following_list,
     }
+    _check_login(request, context)
     return render(request, 'following.html', context)
 
 def addnew(request, userid):
     context = {
-        'uid': userid,
+        'uid': str(userid),
     }
+    _check_login(request, context)
     return render(request, 'addnew.html', context)
 
 def doadd(request):
     userid = int(request.POST.get('userid'))
     videotitle = request.POST.get('videotitle').replace("'", "`").encode('utf-8')
+    ac = request.POST.get('account')
+    pw = request.POST.get('password')
     try:
         videopath = request.FILES['videopath']
     except:
@@ -72,31 +94,47 @@ def doadd(request):
          return HttpResponse('<html><head><META HTTP-EQUIV="refresh" CONTENT="3;URL=user/%s"></head>Add video title failed\n%s</html>' \
          %(userid, str(userid)+","+videoid+","+videotitle))
     url = s.finishUpload(videoid)
+    msg = "videotitle" + " " + burl + videoid
+    snapshot = burl + videoid + "/" + videoid + ".jpeg"
     try:
-        from weibo import APIClient
-        import urllib,httplib,sys
-        APP_KEY = '997501600' 
-        APP_SECRET = 'f236cdb4c1fbc8d243ab580c115ac9e1'  
-        CALLBACK_URL = 'http://liveshooter.cn.mu/weibo_auth/callback.php'
-        burl = 'http://54.248.182.51/'
-        msg = videotitle + " " + burl + videoid
-        snapshot = burl + videoid + "/" + videoid + ".jpeg"
-        ACCOUNT = request.POST.get('account')
-        PASSWORD = request.POST.get('password')
-        client = APIClient(app_key=APP_KEY, app_secret=APP_SECRET, redirect_uri=CALLBACK_URL)
-        conn = httplib.HTTPSConnection('api.weibo.com')
-        postdata = urllib.urlencode({'client_id':APP_KEY,'response_type':'code','redirect_uri':CALLBACK_URL,'action':'submit',
-        'userId':ACCOUNT,'passwd':PASSWORD,'isLoginSina':0,'from':'','regCallback':'','state':'','ticket':'','withOfficalFlag':0})
-        conn.request('POST','/oauth2/authorize',postdata,{'Referer':client.get_authorize_url(),
-        'Content-Type':'application/x-www-form-urlencoded'})
-        res = conn.getresponse()
-        code = res.getheader('location').split('=')[1]
-        conn.close()
-        r = client.request_access_token(code)
-        client.set_access_token(r.access_token, r.expires_in)
-        client.statuses.upload.post(status=msg, pic=urllib.urlopen(snapshot))
-        message = "</br>Video link posted on your weibo successfully"
+        wb = sina.Weibo()
+        if wb.auth(ac, pw):
+            wb.post(msg, snapshot)
+            message = "</br>Video link posted on your weibo successfully"
+        else:
+            message = "</br>Failed to post on your weibo because of wrong account password"
     except:
-        message = "</br>Video link failed to post on your weibo"
+        message = "</br>Failed to post on your weibo because of illegal words"
+    return HttpResponse(message)
     return HttpResponse('''<html><head><META HTTP-EQUIV="refresh" CONTENT="3;URL=user/%s"></head>
     Your video can be watched here: %s,%s</html>''' %(userid, url, message))
+
+def login(request):
+    context = {}
+    _check_login(request, context)
+    return render(request, 'login.html', context)
+
+def dologin(request):
+    logined = 0
+    ac = request.POST.get('account')
+    pw = request.POST.get('password')
+    site = request.POST.get('site')
+    if site == 'sina':
+        wb = sina.Weibo()
+        if wb.auth(ac, pw):
+            nickname, alink = wb.profile()
+            logined = 1
+    if logined:
+        uid = s.loginUser(ac, site, nickname, alink)
+        request.session['login_user'] = nickname
+        request.session['login_id'] = uid
+        return HttpResponse('''<html><head><META HTTP-EQUIV="refresh" CONTENT="3;URL=user/%s"></head>
+        You have logined as %s</html>''' %(request.session['login_id'], request.session['login_user']))
+    else:
+        return HttpResponse('''<html><head><META HTTP-EQUIV="refresh" CONTENT="3;URL=login"></head>login failed</html>''')
+
+def logout(request):
+    request.session['login_user'] = ""
+    request.session['login_id'] = ""
+    return HttpResponse('''<html><head><META HTTP-EQUIV="refresh" CONTENT="3;URL=/"></head>logout successfully!''')
+
